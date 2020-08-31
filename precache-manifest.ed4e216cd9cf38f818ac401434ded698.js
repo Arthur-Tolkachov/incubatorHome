43fe02e1513787af4c4e7df7a70cbef73ba77c9e
Arthur-Tolkachov <babanjerelo@gmail.com>
2020-08-31 13:41:38 +0300

    Updates

---
+++ precache-manifest.ed4e216cd9cf38f818ac401434ded698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22dee6c5ee41787068a80cbbede54fe",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "776e0087580b6685d4ec",
+    "url": "/my-app/static/css/main.41056914.chunk.css"
+  },
+  {
+    "revision": "7d33d397751e6c833dbe",
+    "url": "/my-app/static/js/2.36189a0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.36189a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "776e0087580b6685d4ec",
+    "url": "/my-app/static/js/main.5ab4967f.chunk.js"
+  },
+  {
+    "revision": "67b783608a3cb8753ca2",
+    "url": "/my-app/static/js/runtime-main.d6222abf.js"
+  }
+]);
